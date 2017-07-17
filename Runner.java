cdbc143fb48357adf6f06815f444b848c98920fd
ahill6 <ahill6@ncsu.edu>
2017-07-17 13:51:42 -0400

    Update Runner.java

--- Runner.java
+++ Runner.java
@@ -1,5 +1,8 @@
 package gov.nasa.jpf.symbc.andy;
 
+import java.io.File;
+import java.util.ArrayList;
+
 import gov.nasa.jpf.Config;
 import gov.nasa.jpf.JPF;
 import gov.nasa.jpf.JPFConfigException;
@@ -38,65 +41,49 @@ public class Runner {
 		}
 		return res;
 	}
+	
+	private static String makeClasspath(File f){
+		String classpth = "";
+		//System.out.println(f);
+		
+		// make a file from a
+		if(f.isDirectory()){
+			// if at least one class file, add it to classpth
+			for (File g : f.listFiles()){
+				if (g.isDirectory()){
+					classpth += makeClasspath(g) + ";"; 
+				}
+			}
+			String ttmp = f.getAbsolutePath().replace("/Users/ahill6/Documents/workspace/jpf-symbc", "${jpf-symbc}");
+			//classpth += "${jpf-symbc}/../../../../.."+ttmp;
+			classpth += ttmp;
+		}
+		// recursively check all sub-folders (assuming a is a folder)
+		// if there is a .class in that folder, add it to the classpath
+		//System.out.println(classpth);
+		return classpth;
+	}
+	
 
 	public static void main(String[] args){ 
-		
-		//Z3 prefix input?
-		
-		/*
-		Context ctx = new Context();
-		Expr a = ctx.mkIntConst("a");
-		Expr b = ctx.mkIntConst("b");
-		Expr c = ctx.mkInt("2");
-		Expr g = ctx.mkInt(3);
-		List<BoolExpr> tester = new ArrayList<BoolExpr>();
-		List<BoolExpr> tester2 = new ArrayList<BoolExpr>();
-		BoolExpr d = ctx.mkGt(ctx.mkAdd((ArithExpr)a,(ArithExpr)c), (ArithExpr)b);
-		BoolExpr e = ctx.mkGt(ctx.mkAdd((ArithExpr)a,(ArithExpr)c), ctx.mkInt(2));
-		BoolExpr f = ctx.mkAnd(d,e);
-		tester.add(d);
-		tester.add(e);
-		tester.add(f);
-		
-		for (int i = 0; i < tester.size(); i++){
-			BoolExpr kk = tester.get(i);
-			System.out.println(kk);
-			kk = (BoolExpr) kk.substitute(a, b);
-			System.out.println(kk);
-			tester2.add(kk);
-		}
-		for (BoolExpr kk : tester2){
-			System.out.println(kk);
-		}
-		
-		System.exit(1);
-		*/
-		
-		//Expr a = ctx.mkEq((ArithExpr)ctx.mkIntConst("math.abs(100)"), (ArithExpr) ctx.mkIntConst("100")).simplify();
-		
-		
-		
 		Runner runme = new Runner();
+		String targetPreface = "IntroClassJavaCorrect";
 		
-		/*
-		Methods[] methods = {runme.new Methods("median1a", 3), runme.new Methods("median1c", 3),
-				runme.new Methods("median1b", 3), runme.new Methods("median1d", 3), runme.new Methods("notMedian", 3),
-				runme.new Methods("arrayMedian", 3), runme.new Methods("buggyMedian1ResultChange", 3),
-				runme.new Methods("buggyMedian2ResultChange", 3), runme.new Methods("buggyMedian1StructuralChange", 3),
-				runme.new Methods("buggyMedian2StructuralChange", 3)};
-		
-		
-		Methods[] methods = {runme.new Methods("testMe2", 3), runme.new Methods("factorial", 1),
-				runme.new Methods("sumOrDifference", 3), runme.new Methods("hanoi", 4), runme.new Methods("grade", 3),
-				runme.new Methods("returnMin1", 3), runme.new Methods("returnMin2", 3),
-				runme.new Methods("copies", 3), runme.new Methods("blackjack", 3),
-				runme.new Methods("makeChange", 3), runme.new Methods("xor", 3), runme.new Methods("xorTruths", 3)};
-		*/
-		//, runme.new Methods("addOneConstraint", 3),	runme.new Methods("makeClause", 1)};
-		
-		Methods[] methods = {runme.new Methods("testMe2", 3)};
+		Methods[] methods = {runme.new Methods("median",3),runme.new Methods("medianResultChange",3),runme.new Methods("medianStructuralChange",3),
+		runme.new Methods("medianRewrite",3),runme.new Methods("buggyMedian4ResultChange",3),runme.new Methods("notMedian",3),
+		runme.new Methods("sumOrDifference",3),runme.new Methods("sumOrDifferenceResultChange",3),runme.new Methods("sumOrDifferenceRewrite",3),
+		runme.new Methods("testMe2",3),runme.new Methods("testMe2ResultChange",3),runme.new Methods("testMe2StructuralChange",3),
+		runme.new Methods("factorial",1),runme.new Methods("factorialResultChange",1),runme.new Methods("factorialBiggerResultChange",1),
+		runme.new Methods("factorialRewrite",1),
+		runme.new Methods("grade",3),runme.new Methods("gradeResultChange",3),runme.new Methods("gradeStructuralChange",3),
+		runme.new Methods("minBasic",3),runme.new Methods("minResultChange",3),runme.new Methods("minStructuralChange",3),runme.new Methods("minRewrite",3),
+		runme.new Methods("copies",3),runme.new Methods("copiesResultChange",3),runme.new Methods("copiesStructuralChange",3),runme.new Methods("copiesRewrite",3),
+		runme.new Methods("blackjack",3),runme.new Methods("blackjackResultChange",3),runme.new Methods("blackjackStructuralChange",3),runme.new Methods("blackjackRewrite",3),
+		runme.new Methods("makeChange",3),runme.new Methods("makeChangeResultChange",3),runme.new Methods("makeChangeStructuralChange",3),
+		runme.new Methods("makeChangeRewrite",3),runme.new Methods("xor",3),runme.new Methods("xorResultChange",3),runme.new Methods("xorStructuralChange",3),
+		runme.new Methods("xorRewrite",3),runme.new Methods("xorTruths",3),runme.new Methods("xorTruthsResultChange",3),runme.new Methods("xorTruthsStructuralChange",3),
+		runme.new Methods("xorTruthsRewrite",3)};
 
-		//Methods[] methods = {runme.new Methods("testMe3", 7), runme.new Methods("testMe4", 7), runme.new Methods("testMe5", 7)};
 		try {
 			//for (int  j = 0; j < classes.length(); j++){
 				//methods = classes[j].getMethods();
@@ -106,13 +93,14 @@ public class Runner {
 				Config conf = JPF.createConfig(args);
 	
 				// ... modify config according to your needs
-				conf.setProperty("target", "ToyTests");
+				conf.setProperty("target", targetPreface);
+				//conf.setProperty("target", "org.apache.commons.math3.complex.Complex");
 				conf.setProperty("classpath", "${jpf-symbc}/build/examples");
-				conf.setProperty("sourcepath", "${jpf-symbc}/src/examples");
+				//conf.setProperty("sourcepath", "${jpf-symbc}/src/examples");
 				//conf.setProperty("target", classes[j].className);
 				//conf.setProperty("target", "ToyTests");
 				//conf.setProperty("classpath", "${jpf-symbc}/build/examples");
-				//conf.setProperty("sourcepath", "${jpf-symbc}/src/examples");
+				//conf.setProperty("sourcepath", sourcep);
 	
 				conf.setProperty("symbolic.dp", "no_solver");
 				//conf.setProperty("symbolic.dp", "yices");
@@ -131,21 +119,7 @@ public class Runner {
 					
 					// Write new main for this
 	
-					// write("public static void main(String args[]){");
-					// (new TestPaths()).testMe(
-					// put in default values for all variables that are the appropriate type.
-					// );}
-					/*
-					switch(type)
-					{
-					case String: write("\"\"");
-						break;
-					case boolean: write("false")
-						break;
-					default: write((type)0);
-						break;
-					}
-					*/
+
 					//
 					
 					// Go
@@ -176,5 +150,18 @@ public class Runner {
 			System.out.println("JPF Exception");
 			System.out.println(jx);
 		}
+		
+		ArrayList<String> methodNames = new ArrayList<String>();
+		for (Methods m : methods){
+			methodNames.add(m.getName());
+		}
+		
+		int n = 5;
+		
+		//DBInteractions db = new DBInteractions();
+		//db.nNearest(methodNames.toArray(new String[methodNames.size()]), n, targetPreface);
+		//System.exit(1);
+		DistanceCalculations d = new DistanceCalculations();
+		d.someMethodHere(methodNames.toArray(new String[methodNames.size()]), targetPreface);	
 	}
 }
